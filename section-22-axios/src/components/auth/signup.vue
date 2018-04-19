0d41092754c95cc7b323c48f265ee3b623105dfe
Nick van der Sangen <vandersangen@x-ingredient.nl>
2018-04-19 09:45:55 +0200

    Section 24 - Adding Validation UI Feedback

--- section-22-axios/src/components/auth/signup.vue
+++ section-22-axios/src/components/auth/signup.vue
@@ -2,14 +2,15 @@
   <div id="signup">
     <div class="signup-form">
       <form @submit.prevent="onSubmit">
-        <div class="input">
+        <div class="input" :class="{invalid: $v.email.$error}">
           <label for="email">Mail</label>
           <input
                   type="email"
                   id="email"
                   v-model="email"
                   @input="$v.email.$touch()">
-                  <div> {{ $v }} </div>
+                  <p v-if="!$v.email.email">Please provide a valid emailaddress</p>
+                  <p v-if="!$v.email.required">This field must not be empty</p>
         </div>
         <div class="input">
           <label for="age">Your Age</label>
@@ -166,6 +167,14 @@
   .input select {
     border: 1px solid #ccc;
     font: inherit;
+  }
+
+  .input.invalid label {
+    color: red;
+  }
+  .input.invalid input {
+    border: 1px solid red;
+    background-color: rgb(240, 188, 188);
   }
 
   .hobbies button {
