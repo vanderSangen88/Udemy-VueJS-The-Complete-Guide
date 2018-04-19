f54a30470fd0999d01ed6542120dff769786757f
Nick van der Sangen <vandersangen@x-ingredient.nl>
2018-04-19 10:05:43 +0200

    Section 24 - More Validators

--- section-22-axios/src/components/auth/signup.vue
+++ section-22-axios/src/components/auth/signup.vue
@@ -8,16 +8,19 @@
                   type="email"
                   id="email"
                   v-model="email"
-                  @input="$v.email.$touch()">
+                  @blur="$v.email.$touch()">
                   <p v-if="!$v.email.email">Please provide a valid emailaddress</p>
                   <p v-if="!$v.email.required">This field must not be empty</p>
         </div>
-        <div class="input">
+        <div class="input" :class="{invalid: $v.age.$error}">
           <label for="age">Your Age</label>
           <input
                   type="number"
                   id="age"
-                  v-model.number="age">
+                  v-model.number="age"
+                  @blur="$v.age.$touch">
+                  <p v-if="!$v.age.minVal">You have to be at least {{ $v.age.$params.minVal.min }} years old </p>
+                  <!-- <div>{{ $v.age.$params }}</div> -->
         </div>
         <div class="input">
           <label for="password">Password</label>
@@ -73,7 +76,7 @@
 
 <script>
   import axios from 'axios';
-  import { required, email } from 'vuelidate/lib/validators'
+  import { required, email, numeric, minValue } from 'vuelidate/lib/validators'
 
   export default {
     data () {
@@ -91,6 +94,11 @@
       email: {
         required,
         email
+      },
+      age: {
+        required,
+        numeric,
+        minVal: minValue(18) // Special function which requires a value to be configured
       }
     },
     methods: {
