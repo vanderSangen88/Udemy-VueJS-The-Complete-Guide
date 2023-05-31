8544dbdc5c40d39312f0ca5f576318c63e55e4cd
vanderSangen88 <nick.vandersangen@gmail.com>
2023-05-31 13:20:38 +0200

    clean up

--- section-15 - Connecting to Servers via Http/src/routes.js
+++
@@ -1,7 +0,0 @@
-import Home from './components/Home.vue';
-import Movie from './components/Movie.vue';
-
-export const routes = [
-    { path: '', component: Home }, 
-    { path: '/movie/:imdbID', component: Movie }
-];
